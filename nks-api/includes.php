841e262f34c5d8fdc20bfd976aded10a53374432
artem <artem.the.freelancer@gmail.com>
2023-03-24 20:20:22 +0400

    Mogin endpoints into another place

--- nks-api/includes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once( "inc/errorHandler.php" );
-require_once( "inc/requestManager.php" );
-require_once( "inc/dbConnection.php" );
-require_once( "inc/customerController.php" );
-require_once( "inc/customerModel.php" );
-require_once( "inc/addressModel.php" );
-
